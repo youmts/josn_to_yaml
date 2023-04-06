9e655d2321c100e56d81128e7efa05f32142ba22
Yo Matsumoto <youmts22@gmail.com>
2023-04-06 09:30:33 +0900

    feat: json to yaml function

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
-pub fn json_to_yaml(json: String) -> String {
-    json
+use std::collections::HashMap;
+
+use serde_json::Value;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum LibError {
+    #[error("serde_json error")]
+    SerdeJson(#[from] serde_json::Error),
+    #[error("serde_yaml error")]
+    SerdeYaml(#[from] serde_yaml::Error),
+}
+
+pub fn json_to_yaml(json_string: String) -> Result<String, LibError> {
+    let value_hash_map: HashMap<String, Value> = serde_json::from_str(&json_string)?;
+    let yaml_string = serde_yaml::to_string(&value_hash_map)?;
+
+    Ok(yaml_string)
 }
--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,15 @@ use thiserror::Error;
 enum Error {
     #[error("io error")]
     IO(#[from] io::Error),
+    #[error("lib error")]
+    Lib(#[from] json_to_yaml::LibError),
 }
 
 fn main() -> Result<(), Error> {
     let mut input = "".to_owned();
     io::stdin().read_to_string(&mut input)?;
 
-    let output = json_to_yaml(input);
+    let output = json_to_yaml(input)?;
 
     println!("{}", output);
 
